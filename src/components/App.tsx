db2a6882eb12ab2db61b05818cd896a25d560d8a
hayatsu_wataru <hayatsu.wataru@adways.net>
2018-02-09 20:32:34 +0900

    [add]ステータスを変更する機能を追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Todo } from '../model/todo';
-import Card from './Card';
+import { Status, Todo } from '../model/todo';
+import { Card } from './Card';
 
 /* css */
 import '../css/App.css';
 import '../css/list.css';
 
 class App extends React.Component {
-  public state: {todos:Todo[]};
-  constructor(props:Todo) {
+  public state: {todos:Todo[]}
+  constructor(props:any) {
     super(props);
     this.state = {
       todos: [new Todo("Eat ramen")]
@@ -18,7 +18,7 @@ class App extends React.Component {
   }
 
   public addTodo() {
-    const inputElm: HTMLInputElement = ReactDOM.findDOMNode(this.refs.description) as HTMLInputElement;
+    const inputElm = ReactDOM.findDOMNode(this.refs.description) as HTMLInputElement;
     if(inputElm.value === "") return;
     this.state.todos.push(new Todo(inputElm.value))
     this.setState({
@@ -27,6 +27,12 @@ class App extends React.Component {
     inputElm.value = "";
   }
 
+  public changeStatus(todo:Todo, cardstatus:Status) {
+    todo.status = cardstatus;
+    this.setState({
+      todos: this.state.todos
+    })
+  }
 
   public render() {
     return (
@@ -38,15 +44,21 @@ class App extends React.Component {
           <div className="list active">
             <h2>Active</h2>
             {this.state.todos.map((todo, index) => {
-              return todo.status === 0
-              ? <Card key={index} todo={todo} />: "";
+              return todo.status === Status.Active
+              ? <Card key={index} todo={todo}
+                  changeStatus={this.changeStatus.bind(this)}
+                />
+              : "";
             })}
           </div>
           <div className="list completed">
             <h2>Completed</h2>
             {this.state.todos.map((todo, index) => {
-              return todo.status === 1
-              ? <Card key={index} todo={todo} />: "";
+              return todo.status === Status.Completed
+              ? <Card key={index} todo={todo}
+                  changeStatus={this.changeStatus.bind(this)}
+                />
+              : "";
             })}
           </div>
         </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
-import { Todo } from '../model/todo';
+import { Todo, Status } from '../model/todo';
 import '../css/card.css';
 
 export interface Props {
-  todo: Todo;
+  todo: Todo,
+  changeStatus: Function
 }
 
-export default function Card({todo}: Props) {
-  return (
-    <div className="card">
-      <h3 className="card_description">{todo.description}</h3>
-      <button className="complete_button">completed</button>
-    </div>
-  )
+export class Card extends React.Component<Props> {
+  render(){
+    return (
+      <div className="card">
+        <h3 className="card_description">{this.props.todo.description}</h3>
+        <div className="card_closeButton"></div>
+        <button className="card_button is-active" ref="active" onClick={() => this.props.changeStatus(this.props.todo, Status.Active)}>Active</button>
+        <button className="card_button is-completed" ref="completed" onClick={() => this.props.changeStatus(this.props.todo, Status.Completed)}>Completed</button>
+      </div>
+    )
+  }
 }
