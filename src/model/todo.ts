60ca2090b8d15bb7d5ea5d367d8db852f09aa5e1
hayatsu_wataru <hayatsu.wataru@adways.net>
2018-02-14 12:17:36 +0900

    [add]タスクの期限の設定を追加

--- src/model/todo.ts
+++ src/model/todo.ts
@@ -7,9 +7,11 @@ export class Todo {
   public description: string;
   public status = Status.Active;
   public editable = false;
+  public taskDeadline: string;
 
-  constructor( description: string ) {
+  constructor( description: string, taskDeadline:string ) {
     this.description = description
+    this.taskDeadline = taskDeadline
   }
   public toCompleted(){
     this.status = Status.Completed
