6beee82b6b906070a8e576846bdb9f94ce9af2d8
hayatsu_wataru <hayatsu.wataru@adways.net>
2018-02-27 19:43:17 +0900

    [add]インターフェースに日付を追加

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,46 +3,42 @@ let nextTodoId = 0;
 export interface AddTodoAction {
   type: string;
   id: number;
-	description: string;
+  description: string;
+  date: Date
 }
 
 export interface DeleteTodoAction {
-	type: string;
+  type: string;
   id: number;
 }
-export interface EditTodoAction {
-	type: string;
-	id: number;
-	editable:boolean;
+
+export const addTodo = (description: string, date: Date): AddTodoAction => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    description,
+    date
+  }
 }
 
-
-export const addTodo = (description: string) => {
-	return {
-		type: 'ADD_TODO',
-		id: nextTodoId++,
-		description
-	}
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: 'DELETE_TODO',
+    id
+  }
 }
 
-export const deleteTodo = (id: number) => {
-	return {
-		type: 'DELETE_TODO',
-		id
-	}
-}
-
-export const editTodo = (id: number, description:string) => {
-	return {
-		type: 'EDIT_TODO',
+export const editTodo = (id: number, description: string) => {
+  return {
+    type: 'EDIT_TODO',
     id,
     description
-	}
+  }
 }
 
 export const toggleEditable = (id: number) => {
-	return {
-		type: 'TOGGLE_EDITABLE',
-		id
-	}
+  return {
+    type: 'TOGGLE_EDITABLE',
+    id
+  }
 }
