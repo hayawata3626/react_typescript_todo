7b24d43c94f40eb0b0c525110deb6fd8bdb80e65
hayawata3626 <top.men09@gmail.com>
2018-02-28 10:13:18 +0900

    [mod]編集ボタンを追加

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -12,14 +12,15 @@ interface ItemProps {
 const TodoItem = ({ todo, onDeleteTodo, onEditTodo, onToggleTodoEditable }: ItemProps) => {
   const date:string = todo.date.toString();
   return (
-    <li className="todo" onDoubleClick={() => onToggleTodoEditable(todo.id)}>
+    <li className="todo">
       {todo.editable
-        ? <input type="text" defaultValue={todo.description} onBlur={(e) => onToggleTodoEditable(todo.id)} onChange={(e) => onEditTodo(todo.id, e.target.value)} />
+        ? <input type="text" className="todo_editarea" defaultValue={todo.description} onBlur={(e) => onToggleTodoEditable(todo.id)} onChange={(e) => onEditTodo(todo.id, e.target.value)} />
         : <h2>{todo.description}</h2>
       }
+      <p className="todo_editButton" onClick={() => onToggleTodoEditable(todo.id)}>編集</p>
       <p className="todo_date">{date}</p>
       <div className="todo_closeButton" onClick={() => onDeleteTodo(todo.id)} >ｘ</div>
-      <select name="priority" id="name">
+      <select className="todo_priority" name="priority" id="name">
         <option value="大">大</option>
         <option value="中">中</option>
         <option value="小">小</option>
